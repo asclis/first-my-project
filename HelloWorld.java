2c28979bfefac4a5061c42a008be1994999bf184
ascl <jessemin.s@gmail.com>
2022-05-16 12:17:05 +0100

    first-commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+        System.out.println("배가고프네요옹");
+    }
+}
